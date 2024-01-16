814b02147209d61a63ac7acb6706d870cc2ecfb5
Jonah T <jtollefson8@gmail.com>
2024-01-16 08:20:33 +0100

    not blankable but nullable posters and gave a hardcoded no poster available url.

--- src/embed-resources.js
+++ src/embed-resources.js
@@ -3,12 +3,14 @@ const {EmbedBuilder} = require("discord.js");
 const logger = require('winston');
 
 function embedTMDBShow(client, show) {
-    let imageUrl = "";
+    let imageUrl = null;
     if (show.poster_path !== null && show.poster_path.length !== 0) {
         imageUrl = `https://www.themoviedb.org/t/p/w58_and_h87_face/${show.poster_path}`
     } else if (show.backdrop_path !== null && show.backdrop_path.length !== 0) {
         imageUrl = `https://www.themoviedb.org/t/p/w58_and_h87_face/${show.backdrop_path}`
-    }
+    } else {
+		imageUrl = `https://www.themoviedb.org/t/p/w58_and_h87_face/uc4RAVW1T3T29h6OQdr7zu4Blui.jpg`
+	}
 	
 	console.log(client.user.avatarURL())
     return new EmbedBuilder()
@@ -26,12 +28,14 @@ function embedTMDBShow(client, show) {
 }
 
 function embedTMDBMovie(client, movie) {
-    let imageUrl = "";
+    let imageUrl = null;
     if (movie.poster_path !== null && movie.poster_path.length !== 0) {
         imageUrl = `https://www.themoviedb.org/t/p/w58_and_h87_face/${movie.poster_path}`
     } else if (movie.backdrop_path !== null && movie.backdrop_path.length !== 0) {
         imageUrl = `https://www.themoviedb.org/t/p/w58_and_h87_face/${movie.backdrop_path}`
-    }
+    } else {
+		imageUrl = `https://www.themoviedb.org/t/p/w58_and_h87_face/uc4RAVW1T3T29h6OQdr7zu4Blui.jpg`
+	}
 
     return new EmbedBuilder()
         .setColor("DarkBlue")
